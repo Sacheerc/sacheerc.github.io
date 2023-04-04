101343fdeae45b9a327a30219a74bf6b6261dad1
sacheerc <madushansachintha@gmail.com>
2023-04-04 22:51:10 +0530

    Implemented projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import CustomButton from './CustomButton';
+import CustomText from './CustomText';
+import project1 from '../assets/custom_html_landing_page_2.jpg';
+import project2 from '../assets/portfolio_website_1.jpg';
+import project3 from '../assets/portfolio_website_2.jpg';
+import project4 from '../assets/cover_4.jpg';
+
+const projects = [
+  {
+    title: 'Modern Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: project1,
+    demoLink: '',
+  },
+  {
+    title: 'Modern Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: project2,
+    demoLink: '',
+  },
+  {
+    title: 'Modern Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: project3,
+    demoLink: '',
+  },
+  {
+    title: 'Modern Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: project4,
+    demoLink: '',
+  },
+];
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const projectItem = {
+  width: '90%',
+  padding: 15,
+};
+
+const image = {
+  width: '100%',
+  borderRadius: 10,
+  marginBottom: 15,
+};
+
+export const Projects = ({ theme }) => {
+  return (
+    <div className='section'>
+      <div
+        className='text-center sub-heading'
+        style={{ color: theme.darkerColor }}
+      >
+        Projects
+      </div>
+      <div>
+        <Carousel
+          responsive={responsive}
+          containerClass='carousel-container'
+          itemClass='carousel-item-padding-40-px'
+          infinite={true}
+        >
+          {projects &&
+            projects.map((project, index) => (
+              <div key={index} className='project-item' style={projectItem}>
+                <img src={project.image} alt='project 1' style={image}></img>
+                <div>
+                  <CustomText
+                    fontSize={18}
+                    fontWeight={600}
+                    marginBottom={10}
+                    color={theme.darkColor}
+                  >
+                    {project.title}
+                  </CustomText>
+                </div>
+                <div style={{ marginTop: 5, marginBottom: 10 }}>
+                  <CustomText
+                    fontSize={16}
+                    fontWeight={400}
+                    color={theme.lightColor}
+                  >
+                    {project.description}
+                  </CustomText>
+                </div>
+                <CustomButton
+                  backgroundColor={theme.primaryColor}
+                  color={theme.lighterColor}
+                >
+                  Demo
+                </CustomButton>
+              </div>
+            ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,18 @@ import { BioSection } from '../components/BioSection';
 import { Banner } from '../components/Banner';
 import { Skills } from '../components/Skills';
 import { Experience } from '../components/Experience';
+import { Projects } from '../components/Projects';
 
 export const Home = () => {
   const { theme } = React.useContext(ThemeContext);
   return (
     <>
-      <IntroSection theme={theme} />
-      <BioSection theme={theme} />
+      <IntroSection id='intro-section' theme={theme} />
+      <BioSection id='about-section' theme={theme} />
       <Banner theme={theme} />
       <Skills theme={theme} />
       <Experience theme={theme} />
+      <Projects theme={theme} />
     </>
   );
 };
